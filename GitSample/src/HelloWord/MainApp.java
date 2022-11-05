3b14a5ab2b890f6f34918616604b968ed9f7c92e
Ambrose <nbailove3@gmail.com>
2022-11-05 14:52:39 +0800

    3nd Version

--- GitSample/src/HelloWord/MainApp.java
+++ GitSample/src/HelloWord/MainApp.java
@@ -4,7 +4,17 @@ public class MainApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-   System.out.print("2nd Version");
+   System.out.print("3nd Version");
+   
+   Math cMath = new Math();
+   System.out.println();
+   System.out.print(cMath.add(5, 5));
 	}
 
+}
+
+class Math{
+	int add(int x, int y) {
+		return x+y;
+	}
 }
